d4fe15e7926a7c0b13504f8f2243ea5874cdef60
meltrust <original.mtapia@outlook.com>
2021-02-18 05:27:58 -0600

    add instance variables and private methods

--- lib/presenter.rb
+++ lib/presenter.rb
@@ -3,23 +3,23 @@ require_relative '../lib/scraper'
 class Presenter < ScrapePage::ScrapeOffers
   include ScrapePage
 
-  def present_results
-    count = 0
+  def present_deals
+    @count = 0
 
     offers_array_builder.each do |h|
-      puts "#{count + 1}. " + h[:title].yellow.bold
+      puts "#{@count + 1}. " + h[:title].yellow.bold
       puts h[:original_price].blue
       puts h[:discount].green.bold
       puts h[:price].green.bold
       puts h[:url]
       puts
-      count += 1
-      next unless (count % 5).zero?
+      @count += 1
+      next unless (@count % 5).zero?
 
       puts
-      puts 'Press any key to continue...' unless count == 40
+      puts 'Press any key to continue...' unless @count == 40
 
-      $stdin.getch unless count == 40
+      $stdin.getch unless @count == 40
       puts
     end
   end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,34 +6,36 @@ require 'spinning_cursor'
 
 module ScrapePage
   class ScrapeOffers
+    private
+
     def offers_scraper
       SpinningCursor.run do
         message 'Done!'
       end
-      parsed_page = Nokogiri::HTML(HTTParty.get('https://store.steampowered.com/').body)
+      @parsed_page = Nokogiri::HTML(HTTParty.get('https://store.steampowered.com/').body)
       SpinningCursor.stop
-      parsed_page.css('div#tab_specials_content a') # 40 deals
+      @parsed_page.css('div#tab_specials_content a') # 40 deals
     end
 
     def offers_array_builder
-      deal_listings = offers_scraper
-      deals = []
+      @deal_listings = offers_scraper
+      @deals = []
       i = 0
       while i <= 39
-        deal_listings.each do |deal_listing| # iterate through all listings and build a hash with data from each
-          deal = {
+        @deal_listings.each do |deal_listing| # iterate through all listings and build a hash with data from each
+          @deal = {
             title: deal_listing.css('div.tab_item_name').text,
             original_price: deal_listing.css('div.discount_original_price').text,
             discount: deal_listing.css('div.discount_pct').text,
             price: deal_listing.css('div.discount_final_price').text,
-            url: deal_listings.css('a')[i].attribute('href').value
+            url: @deal_listings.css('a')[i].attribute('href').value
           }
 
-          deals << deal # put all deals into an array called deals.
+          @deals << @deal # put all deals into an array called deals.
           i += 1
         end
       end
-      deals[0..39] # return the first 40 array of deals
+      @deals[0..39] # return the first 40 array of deals
     end
   end
 end
