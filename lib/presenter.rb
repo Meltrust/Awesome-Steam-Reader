6bd2402f84e68bb98319b2235517e07870263b45
meltrust <original.mtapia@outlook.com>
2021-02-18 05:02:16 -0600

    add new file for Presenter class

---
+++ lib/presenter.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/scraper'
+
+class Presenter < ScrapePage::ScrapeOffers
+  include ScrapePage
+
+  def present_results
+    count = 0
+
+    offers_array_builder.each do |h|
+      puts "#{count + 1}. " + h[:title].yellow.bold
+      puts h[:original_price].blue
+      puts h[:discount].green.bold
+      puts h[:price].green.bold
+      puts h[:url]
+      puts
+      count += 1
+      next unless (count % 5).zero?
+
+      puts
+      puts 'Press any key to continue...' unless count == 40
+
+      $stdin.getch unless count == 40
+      puts
+    end
+  end
+end
