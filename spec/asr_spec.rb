29e562704fb64abe406dc746fa9b07f10254ed6a
meltrust <original.mtapia@outlook.com>
2021-02-18 22:21:23 -0600

    fix some typos

--- spec/asr_spec.rb
+++ spec/asr_spec.rb
@@ -1,57 +1,14 @@
-require_relative '../lib/scraper'
+require_relative '../lib/scrapper'
 require_relative '../lib/presenter'
+require 'rspec'
 
-# describe 'MainScraper' do
-#   let(:main_scraper) { ScrapePage::MainScraper.new }
+describe 'PageScraper' do
+  let(:main_scraperr) { Scraper::PageScrapper.new }
+  let(:present_results) { Presenter::OffersPresenter.new }
 
-#   describe '#scraper' do
-#     it 'Returns 40 elements' do
-#       expect(main_scraper.scraper.length).to eq(40)
-#     end
-
-#     it 'Returns an array' do
-#       expect(main_scraper.scraper).to be_an_instance_of Array
-#     end
-
-#     it 'It is not nil' do
-#       expect(main_scraper.scraper[0]).not_to be_nil
-#     end
-#     it 'Has a key called "title"' do
-#       expect(main_scraper.scraper[0]).to have_key(:title)
-#     end
-#     it 'Has a key called "url" that includes "https"' do
-#       expect(main_scraper.scraper[0][:url]).to include('https')
-#     end
-#     it 'It is not empty' do
-#       expect(main_scraper.scraper).not_to be_empty
-#     end
-#   end
-
-describe 'Presenter' do
-  let(:main_presenter) { Presenter::OffersPresenter.new }
-
-  describe '#present_deals' do
-    it 'calls the offer_array_builder method' do
-      mock = double
-      expect(Presenter::OffersPresenter).to receive(main_presenter.present_deals)
+  describe '#page_scraper' do
+    it 'It is not nil' do
+      expect(Scraper.page_scraper).not_to be_nil
     end
-
-    # it 'Is not empty' do
-    #   mock = double
-    #   expect(mock).not_to be_empty
-    # end
-
-    # it 'It is not nil' do
-    #   expect(main_presenter.present_deals).not_to be_nil
-    # end
-    # it 'Is an Array' do
-    #   expect(main_presenter.present_deals).to be_an_instance_of Array
-    # end
-    # it 'Has 40 elements' do
-    #   expect(main_presenter.present_deals).to eq(40)
-    # end
-    # it 'Number of elements divisible by 10' do
-    #   expect(main_presenter.present_deals).to(satisfy { |x| x % 10 == 0 })
-    # end
   end
 end
